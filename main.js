8f43f5e92e596117ead3ec1df1d14f375f71c810
kailachen <kailachen2004@gmail.com>
2024-02-29 20:05:35 -0500

    Fixed things + Preview Box
    Fixed the speed images on the homepage.
    Finished adding all the song links. Created the Preview box on the movie page.

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-let index = 0;
-displayImages();
-function displayImages() {
-  let i;
-  const images = document.getElementsByClassName("image");
-  for (i = 0; i < images.length; i++) {
-    images[i].style.display = "none";
+
+//copied from https://www.educative.io/answers/how-to-create-an-automatic-slideshow-with-html-css-and-js 
+  let index = 0;
+  displayImages();
+  function displayImages() {
+    let i;
+    const images = document.getElementsByClassName("image");
+    for (i = 0; i < images.length; i++) {
+      images[i].style.display = "none";
+    }
+    index++;
+    if (index > images.length) {
+      index = 1;
+    }
+    images[index-1].style.display = "block";
+    setTimeout(displayImages, 4000); 
   }
-  index++;
-  if (index > images.length) {
-    index = 1;
-  }
-  images[index-1].style.display = "block";
-  setTimeout(displayImages, 2000); 
-}
